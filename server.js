df2ed22586a3c632fef44800906d1ff6763072af
Hardik Nakrani <hdnakarani@gmail.com>
2018-12-07 18:39:08 +0530

    setup start script and heroku port

--- server.js
+++ server.js
@@ -1,44 +1,41 @@
 const express = require('express');
 const hbs = require('hbs');
-const fs=require(`fs`);
+const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
 
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFile('server.log', log + '\n',(error)=>
+  {
+    if(error){
+      console.log(error);
+      
+    }
+  });
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
 hbs.registerHelper('getCurrentYear', () => {
-  return new Date().getFullYear()
+  return new Date().getFullYear();
 });
 
 hbs.registerHelper('screamIt', (text) => {
   return text.toUpperCase();
 });
-
-
-app.use((req,res,next)=>{
-  var now=new Date().toString();
-  var log=`${now}:${req.method} ${req.url}`;
-fs.appendFile('server.log',log + '\n',(error)=>{
-  if(error){
-  console.log('Unable to Append to server.log.');
-  }
-});
-console.log(log);
-next();
-});
-
-/* app.use((req,res,next)=>{
-
-  res.render('maintanance.hbs');
- 
-}); */
-
-
-app.use(express.static(__dirname + '/public'));
-
-
-
 
 app.get('/', (req, res) => {
   res.render('home.hbs', {
@@ -60,6 +57,6 @@ app.get('/bad', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Server is up on port 3000');
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
 });
